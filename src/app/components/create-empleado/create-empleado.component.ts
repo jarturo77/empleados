bd66546f129a4037b702218cf4cebb04ec778c77
Josimar Garcia <jo.marturo77@gmail.com>
2021-05-04 16:11:56 -0600

    creacion de metodos de Modificacion de empleados

--- src/app/components/create-empleado/create-empleado.component.ts
+++ src/app/components/create-empleado/create-empleado.component.ts
@@ -1,6 +1,6 @@
 import { EmpleadoService } from './../../services/empleado.service';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 
@@ -13,27 +13,44 @@ export class CreateEmpleadoComponent implements OnInit {
   createEmpleado: FormGroup;
   submitted = false;
   loading = false;
+  id: string | null; //variable de captura de id desde ruta
+  titulo = 'Agregar Empleado';
 
   constructor(private fb: FormBuilder,private _empleadoService: EmpleadoService,
               private router:Router,
-              private toastr: ToastrService) {
+              private toastr: ToastrService,
+              private aRoute: ActivatedRoute    ///libreria para capturar id desde la ruta
+              ) {
     this.createEmpleado=this.fb.group({
       nombre: ['',Validators.required],
       apellido: ['',Validators.required],
       documento: ['',Validators.required],
       salario: ['',Validators.required]
     })
+    this.id = this.aRoute.snapshot.paramMap.get('id');  //Captura de id paraa modificacion
+    console.log(this.id );
+    
    }
 
   ngOnInit(): void {
+    this.esEditar();
   }
-  agregarEmpleado(){
+  agregarEditarEmpleado(){
     this.submitted = true;
 
     if(this.createEmpleado.invalid){
       return;
 
     }
+    if (this.id === null) {
+      this.agregarEmpleado();
+      
+    }else{
+      this.editarEmpleado(this.id);
+    }
+    
+  }
+  agregarEmpleado(){
     const empleado: any ={
       nombre: this.createEmpleado.value.nombre,
       apellido: this.createEmpleado.value.apellido,
@@ -56,7 +73,52 @@ export class CreateEmpleadoComponent implements OnInit {
       this.loading = false;
     
     })
+
+  }
+  
+  editarEmpleado(id: string){
     
+    const empleado: any ={
+      nombre: this.createEmpleado.value.nombre,
+      apellido: this.createEmpleado.value.apellido,
+      documento: this.createEmpleado.value.documento,
+      salario: this.createEmpleado.value.salario,
+
+      fechaActualizacion: new Date()
+      
+
+    }
+    this.loading = true;
+    
+    this._empleadoService.actualizarEmpleado(id, empleado).then(() =>{
+      this.loading = false;
+      this.toastr.info('El empleado fue modificado con exito','Empleado modificado',{
+        positionClass: 'toast-bottom-right'
+      })
+        this.router.navigate(['/list-empleados']);
+    })
+  }
+
+  esEditar(){
+    
+    if (this.id !== null) {
+      this.titulo = 'Editar Empleado';
+      this.loading = true;
+      
+      this._empleadoService.getEmpleado(this.id).subscribe(data =>{
+        this.loading = false;
+        console.log(data);
+        this.createEmpleado.setValue({
+          nombre: data.payload.data()['nombre'],
+          apellido: data.payload.data()['apellido'],
+          documento: data.payload.data()['documento'],
+          salario: data.payload.data()['salario'],
+        })
+        
+        
+        
+      })
+    }
   }
 
 }
--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -23,4 +23,12 @@ export class EmpleadoService {
   eliminarEmpleado(id: string): Promise<any>{
     return this.firestore.collection('empleados').doc(id).delete();
   }
+
+  getEmpleado(id: string): Observable<any>{
+    return this.firestore.collection('empleados').doc(id).snapshotChanges();
+  }
+  actualizarEmpleado(id: string, data:any): Promise<any>{
+    return this.firestore.collection('empleados').doc(id).update(data);
+    
+  }
 }
